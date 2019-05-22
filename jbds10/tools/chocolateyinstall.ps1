792c7e6c938e39c84e3c0835efdebe0126ab918c
umapathy <sungiri88@gmail.com>
2019-05-22 03:20:03 +0000

    JBDS10 chocolatey

---
+++ jbds10/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
+$jarPath = Join-Path -Path $toolsDir -ChildPath 'devstudio-10.0.0.GA-installer-standalone.jar'
+
+$validExitCodes = @(0,21)
+
+$installXml = Join-Path -Path $toolsDir -ChildPath 'InstallConfigRecord.xml'
+$installArgs = "java -jar $($jarPath) $($installXml)"
+ 
+try {
+ 
+ 
+  Start-ChocolateyProcessAsAdmin -Statements "$installArgs" `
+                                 -ExeToRun "powershell" `
+                                 -ValidExitCodes $validExitCodes
+} catch {
+  throw
+}
---
+++ jbds10/tools/chocolateyuninstall.ps1
@@ -0,0 +1,8 @@
+﻿$tools_location = "C:\devstudio"
+$filename = 'uninstaller.jar'
+if ($tools_location.Contains('/')) { $url = "$tools_location/$filename" }
+else { $url = "$tools_location\$filename" }
+
+
+
+Install-BinFile -Name JBDS10 -Path java -Command '-jar','$url'
